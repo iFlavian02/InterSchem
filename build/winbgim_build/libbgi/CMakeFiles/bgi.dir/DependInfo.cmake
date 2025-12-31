8b0eff77db288267387b46269d95402e697e77a3
Flavian <ilieflavian02@gmail.com>
2025-12-31 14:53:52 +0200

    Add GitHub release workflow

---
+++ build/winbgim_build/libbgi/CMakeFiles/bgi.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/dibutil.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/dibutil.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/dibutil.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/drawing.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/drawing.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/drawing.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/file.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/file.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/file.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/misc.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/misc.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/misc.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/mouse.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/mouse.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/mouse.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/palette.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/palette.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/palette.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/text.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/text.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/text.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/winbgi.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/winbgi.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/winbgi.cxx.obj.d"
+  "D:/Facultate/An1/Introducere in programare/InterSchem/headers/Winbgim/libbgi/src/winthread.cxx" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/winthread.cxx.obj" "gcc" "winbgim_build/libbgi/CMakeFiles/bgi.dir/src/winthread.cxx.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/winbgim_build/test/CMakeFiles/testbgi.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for testbgi.
+# This may be replaced when dependencies are built.
